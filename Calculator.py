e1c125ed8789257ad62b5ee52c02ac4b90ea62f8
EricAudizzio <erixaudixio@gmail.com>
2021-06-28 00:15:06 -0300

    probando tdv

--- Calculator.py
+++ Calculator.py
@@ -5,6 +5,6 @@ class Calculator:
     def add(self):
         return self.num1 + self.num2
 
-if __name__ == "__main__":
-    instance = Calculator(1,2)
-    print(instance.add())
+
+instance = Calculator(1,2)
+print(instance.add())
