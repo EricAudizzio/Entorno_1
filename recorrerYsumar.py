b0f7dcf02255ae814d319f7ab8c65953d04d3bf4
EricAudizzio <erixaudixio@gmail.com>
2021-06-27 21:42:52 -0300

    algunos retoques

--- recorrerYsumar.py
+++ recorrerYsumar.py
@@ -5,8 +5,6 @@ data = archivo.to_dict("list")
 # "list" significa que vamos a almacenar a cada columna como una lista con su contenido
 
 print(data)
-print(data['Nombre'])     # Accedemos a los datos de una columna
-print(data['Nombre'][2])  # Accedemos al índice 2 de la columna 'Nombres'
 
 lista = [12, 45, 67, 32, 89, 73, 34, 12, 10]
 sum = 0
