e325a0e92fd443d2c78b17765fa3ffc30cb0a911
EricAudizzio <erixaudixio@gmail.com>
2021-06-29 12:09:10 -0300

    a ver si era eso

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from sys import argv
 import bottle
-from bottle import route, run
+from bottle import route,run
 
 class Calculator:
     def __init__(self, num1, num2):
@@ -23,5 +23,9 @@ def index():
 	return "<h1>Hola Mundo, esto sera una calculadora web!!!</h1>"
 
 
+#if __name__ == '__main__':
+#	run(host='127.0.0.1',port=8000)
+
+
 if __name__ == '__main__':
-	run(host='127.0.0.1',port=8000)
+	run(host='0.0.0.0', port=argv[1])
